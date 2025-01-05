8bc57fe74ce1936894b32d02f8f21c41bcfd101e
Mateusz <mateuszjasi@op.pl>
2025-01-05 19:39:13 +0100

    Product images tweaks

--- products/models.py
+++ products/models.py
@@ -1,11 +1,30 @@
+import os
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validate_image(file):
+    ext = os.path.splitext(file.name)[1].lower()
+    valid_extensions = ['.png', '.jpg', '.jpeg']
+    if ext not in valid_extensions:
+        raise ValidationError(f"Invalid file extension. Allowed extensions are: {', '.join(valid_extensions)}")
+
+
+def product_image_path(instance, filename):
+    ext = os.path.splitext(filename)[1].lower()
+    return f'product_images/{instance.pk}_1{ext}'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.ImageField(
+        upload_to=product_image_path,
+        blank=True,
+        null=True,
+        validators=[validate_image]
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_image_url(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
